66bcacdc14e1b572c07150c2a612da3dfe7f6cb1
Christian John jiménez Calvo <christianjjc@mail.com>
2024-08-27 22:21:09 -0500

    add product-image features ok!  On branch section-11-type-orm-relations  Changes to be committed: 	modified:   src/products/dto/create-product.dto.ts         new file:   src/products/entities/index.ts 	new file:   src/products/entities/product-image.entity.ts         modified:   src/products/entities/product.entity.ts 	modified:   src/products/products.controller.ts 	modified:   src/products/products.module.ts 	modified:   src/products/products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,9 +3,9 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
+import { ProductImage, Product } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -14,39 +14,58 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRepository.create(createProductDto);
+      const { images = [], ...productDetails } = createProductDto;
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map((image) => this.productImageRepository.create({ url: image })),
+      });
       await this.productRepository.save(product);
-      return product;
+      return { ...product, images };
     } catch (error) {
       this.handleDBExceptions(error);
     }
   }
 
-  findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto) {
     const { limit = 10, offset = 0 } = paginationDto;
-    return this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
+      relations: {
+        images: true,
+      },
     });
+    return products.map((product) => ({
+      ...product,
+      images: product.images.map((img) => img.url),
+    }));
+    /*     return products.map(({ images, ...rest }) => ({
+      ...rest,
+      images: images.map((img) => img.url),
+    })); */
   }
 
   async findOne(term: string) {
     let product: Product;
 
     if (isUUID(term)) {
+      // product = await this.productRepository.findOne({ where: { id: term }, relations: { images: true } });
       product = await this.productRepository.findOneBy({ id: term });
     } else {
       // product = await this.productRepository.findOneBy({ slug: term }); //ya no usamos esta línea.
-      const queryBuilder = this.productRepository.createQueryBuilder(); // se reemplaza con esta.
+      const queryBuilder = this.productRepository.createQueryBuilder('prod'); // se reemplaza con esta.
       product = await queryBuilder
         .where('UPPER(title) =:title or slug =:slug', {
           title: term.toUpperCase(),
           slug: term.toLowerCase(),
         })
+        .leftJoinAndSelect('prod.images', 'prodImages')
         .getOne();
     }
     // const product = await this.productRepository.findOneBy({ id });
@@ -56,10 +75,20 @@ export class ProductsService {
     return product;
   }
 
+  //* MÉTODO PARA APLANAR LAS IMAGENES - DEVOLVER SOLO LO QUE NECESITO
+  async findOnePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return {
+      ...rest,
+      images: images.map((img) => img.url),
+    };
+  }
+
   async update(id: string, updateProductDto: UpdateProductDto) {
     const product = await this.productRepository.preload({
       id,
       ...updateProductDto,
+      images: [],
     });
     if (!product) throw new NotFoundException(`Product with id: '${id}' not Found`);
 
