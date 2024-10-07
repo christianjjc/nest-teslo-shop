cc210e9e6f7bb5cf02059a29a974ce66e98b5262
Christian John jiménez Calvo <christianjjc@mail.com>
2024-10-07 16:32:12 -0500

    sect-15-ep05-server-escuchar-conexiones-desconexiones

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.enableCors();
+  app.enableCors({
+    // origin: ['https://betterjavacode.com', 'https://www.google.com'],
+    // methods: ['POST', 'PATCH', 'DELETE', 'GET'],
+  });
+
   const logger = new Logger('Bootstrap');
 
   app.setGlobalPrefix('api');
@@ -29,3 +36,29 @@ async function bootstrap() {
   logger.log(`App Running on Port: ${process.env.PORT}`);
 }
 bootstrap();
+
+//! CORS OPTIONS
+//* Configuring CORS Options
+//* Optionally, you can pass configuration options to the enableCors() method to customize the CORS behavior.
+//* The available configuration options include:
+
+//? origin: A string or an array of strings representing allowed origins.
+//* methods: A string or an array of strings representing allowed HTTP methods.
+//? allowedHeaders: A string or an array of strings representing allowed headers.
+//* exposedHeaders: A string or an array of strings representing headers exposed to the client.
+//? credentials: A boolean value indicating whether to include credentials in the request (e.g., cookies, HTTP authentication).
+//* maxAge: A number representing the maximum age of the preflight request (in seconds).
+
+/*
+ //* async enableCors() {
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+      origin: 'https://example.com',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+      maxAge: 86400,
+    });
+  }
+*/
